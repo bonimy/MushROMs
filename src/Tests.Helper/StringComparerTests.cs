df2b2ef7381d9863da0789031d84cfadb607b9b5
Nelson Garcia <swr.mushroms@gmail.com>
2018-03-04 01:44:06 -0800

    Update some license headers

--- src/Tests.Helper/StringComparerTests.cs
+++ src/Tests.Helper/StringComparerTests.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// <copyright file="StringComparerTests.cs" company="Public Domain">
+//     Copyright (c) 2018 Nelson Garcia. All rights reserved
+//     Licensed under GNU Affero General Public License.
+//     See LICENSE in project root for full license information, or visit
+//     https://www.gnu.org/licenses/#AGPL
+// </copyright>
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace Tests.Helper
--- src/Tests.Helper/SubstringInfoTests.cs
+++ src/Tests.Helper/SubstringInfoTests.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="SubstringPointerTests.cs" company="Public Domain">
+﻿// <copyright file="SubstringInfoTests.cs" company="Public Domain">
 //     Copyright (c) 2018 Nelson Garcia. All rights reserved
 //     Licensed under GNU Affero General Public License.
 //     See LICENSE in project root for full license information, or visit
